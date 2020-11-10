226f05448e36d63cd27a8a9800f7ad96185be2e6
Shrivu Shankar <shrivu1122@gmail.com>
2020-11-10 15:30:48 -0600

    fix api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || window.location.origin;
 
 let getToken = () => {
   return JSON.parse(localStorage.getItem('aiportal:token') || '""');
