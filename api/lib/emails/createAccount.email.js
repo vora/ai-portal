f949e1f077cbde4d149cdde7370c91eaf4c9da73
Martha Czernuszenko <mvczer@gmail.com>
2020-11-19 11:44:16 -0600

    update create account with original template (#34)
    * update create account with original template
    
    Template: https://docs.google.com/document/d/1MCGk8TZpo9d7nncStZbRMbPAdmPeLNSTsUnzZvEkegg/ed
    
    * added space

--- api/lib/emails/createAccount.email.js
+++ api/lib/emails/createAccount.email.js
@@ -1,5 +1,7 @@
 module.exports.subject = ({}) => `AI Global: New Account`;
 module.exports.text = ({ name, verifyUrl }) =>
-  `Hi ${name}. Thanks for creating your account. ${verifyUrl}`;
+  `Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
+    To complete the registration, please verify your email${verifyUrl}`;
 module.exports.html = ({ name, verifyUrl }) =>
-  `<strong>Hi ${name}. Thanks for creating your account. ${verifyUrl}<strong>`;
+  `<strong>Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
+    To complete the registration, please verify your email ${verifyUrl}<strong>`;
