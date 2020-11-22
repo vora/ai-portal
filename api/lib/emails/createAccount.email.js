4758fd1188df1153ae19f5f5c5d8a028db6081e9
Martha Czernuszenko <mvczer@gmail.com>
2020-11-22 10:02:10 -0600

    Added image and spacing (#44)
    * adding space
    
    * Update resetPassword.email.js
    
    * Added image and spacing

--- api/lib/emails/createAccount.email.js
+++ api/lib/emails/createAccount.email.js
@@ -1,7 +1,9 @@
 module.exports.subject = ({}) => `AI Global: New Account`;
 module.exports.text = ({ name, verifyUrl }) =>
   `Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
-    To complete the registration, please verify your email${verifyUrl}`;
+    To complete the registration, please verify your email${verifyUrl} \n
+    Thanks, AI Global`;
 module.exports.html = ({ name, verifyUrl }) =>
   `<strong>Hi ${name}. Welcome to the AI Global portal! Your new account has been created for the AI Global Responsible AI Portal.
-    To complete the registration, please verify your email ${verifyUrl}<strong>`;
+    To complete the registration, please verify your email ${verifyUrl}<strong>
+     <strong><br> Thanks, AI Global <br> <img src="https://portal.dev.ai-global.org/logo.png"/>`;
--- api/lib/emails/resetPassword.email.js
+++ api/lib/emails/resetPassword.email.js
@@ -1,5 +1,7 @@
 module.exports.subject = ({}) => `AI Global: Password Reset`;
 module.exports.text = ({ resetURL }) =>
-  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.`;
+  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password. \n
+   Thanks, AI Global`;
 module.exports.html = ({ resetURL }) =>
-  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong>`;
+  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong>
+   <strong> <br> Thanks, AI Global`;
