a8cd07d0c9c3b1a32551c6f5f994d7a04cb12604
Lucinda Nguyen <44307632+lucindan@users.noreply.github.com>
2020-11-25 11:39:45 -0600

    Lucinda/add topic (#46)
    * Fix login error notification
    
    * simple form
    
    Co-authored-by: Shrivu Shankar <shrivu1122@gmail.com>

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import AddOrganization from './views/AddOrganization';
 import ViewOrganization from './views/ViewOrganization';
 import UserSettings from './views/UserSettings';
 import Mod from './views/Mod';
+import AddTopic from './views/AddTopic';
 import Feedback from './views/Feedback';
 
 function App() {
@@ -41,6 +42,7 @@ function App() {
               path="/organizations/example"
               component={ViewOrganization}
             />
+            <Route exact path="/topics/create" component={AddTopic} />
             <Route exact path="/feedback" component={Feedback} />
           </Switch>
         </BrowserRouter>
