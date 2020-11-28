ad457ee2323723020a505e4d97be4448bd296190
Martha Czernuszenko <mvczer@gmail.com>
2020-11-28 11:54:48 -0600

    added break (#49)
    post-testing

--- api/lib/emails/resetPassword.email.js
+++ api/lib/emails/resetPassword.email.js
@@ -1,7 +1,7 @@
 module.exports.subject = ({}) => `AI Global: Password Reset`;
 module.exports.text = ({ resetURL }) =>
-  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password. \n
+  `You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password. \n \n
    Thanks, AI Global`;
 module.exports.html = ({ resetURL }) =>
-  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong>
+  `<strong>You have requested your password on AI Global to be reset. Visit ${resetURL} to reset your password.<strong><br>
    <strong> <br> Thanks, AI Global<br> <img src="https://portal.dev.ai-global.org/logo.png"/>`;
